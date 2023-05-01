8fbac0c6f897d6247b5e99cf0017820d46523715
Paul <moicoxoxo@gmail.com>
2023-05-01 17:31:00 -0400

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import process from 'process'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import process from 'process';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '~': path.resolve(process.cwd(), './src'),
-      static: path.resolve(process.cwd(), './public/static')
-    }
+      static: path.resolve(process.cwd(), './public/static'),
+    },
   },
-  plugins: [react()]
-})
+  plugins: [react()],
+});
